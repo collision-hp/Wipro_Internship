53c3d25ae733de397a41821bedfc13bda986d536
collision-hp <champatisatyabrata26@gmail.com>
2025-06-20 01:05:06 +0530

    Removed unnecessary files

--- Wipro-Training/NODEJS/1_Node.js
+++
@@ -1,6 +0,0 @@
-console.log("Sample file")
-var i=0;
-while(i<10){
-    console.log(i*5);
-    i++;
-}
--- Wipro-Training/NODEJS/2_Modules.js
+++
@@ -1,8 +0,0 @@
-exports.demo=function(){
-    return "Hello World"
-}
-exports.add=function(a,b){
-    return a*b;
-}
-
-// Non printable
